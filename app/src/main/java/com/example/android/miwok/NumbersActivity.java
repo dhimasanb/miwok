a1074c857403c8615e1fb9f7f71f35e697b234df
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-06 17:11:52 +0700

    Switch to using a list view

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,10 +18,8 @@ package com.example.android.miwok;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -48,23 +46,23 @@ public class NumbersActivity extends AppCompatActivity {
         Log.v("NumbersActivity", "Word at index 0: " + words[0]);
         Log.v("NumbersActivity", "Word at index 1: " + words[1]);
 
-        // Create Array List
-        ArrayList<String> cars = new ArrayList<>();
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        // Initialize elements in array list
-        cars.add("Toyota");
-        cars.add("BMW");
-        cars.add("Mercedes");
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
-
-        int index = 0;
-        while(index<cars.size()) {
-            TextView carView = new TextView(this);
-            carView.setText(cars.get(index));
-            rootView.addView(carView);
-
-            index++; // increment
-        }
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
     }
 }
