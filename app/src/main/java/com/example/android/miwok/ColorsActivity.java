f491b235ff9068a81c89a9af825bc762f76d6ea5
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-07 16:52:29 +0700

    Add image

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -32,14 +32,14 @@ public class ColorsActivity extends AppCompatActivity {
         ArrayList<Word> words = new ArrayList<Word>();
 
         //words.add("one");
-        words.add(new Word("red", "wetetti"));
-        words.add(new Word("green", "chokokki"));
-        words.add(new Word("brown", "takaakki"));
-        words.add(new Word("gray", "topoppi"));
-        words.add(new Word("black", "kululli"));
-        words.add(new Word("white", "kelelli"));
-        words.add(new Word("dusty yellow", "topiisә"));
-        words.add(new Word("mustard yellow", "chiwiitә"));
+        words.add(new Word("red", "wetetti", R.drawable.color_red));
+        words.add(new Word("green", "chokokki", R.drawable.color_green));
+        words.add(new Word("brown", "takaakki", R.drawable.color_brown));
+        words.add(new Word("gray", "topoppi", R.drawable.color_gray));
+        words.add(new Word("black", "kululli", R.drawable.color_black));
+        words.add(new Word("white", "kelelli", R.drawable.color_white));
+        words.add(new Word("dusty yellow", "topiisә", R.drawable.color_dusty_yellow));
+        words.add(new Word("mustard yellow", "chiwiitә", R.drawable.color_mustard_yellow));
 
         // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
         // adapter knows how to create layouts for each item in the list, using the
